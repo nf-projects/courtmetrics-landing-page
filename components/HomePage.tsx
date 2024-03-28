4cdcace99180390309cbdaab789d4084ba3e3a13
nf-projects <90344282+nf-projects@users.noreply.github.com>
2024-03-27 21:51:43 -0700

    Update HomePage layout and remove "Get started" button

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -144,27 +144,9 @@ export default function HomePage() {
 							fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)"
 						/>
 					</svg>
-					<div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-40">
+					<div className="mx-auto max-w-7xl px-6 py-6 sm:py-8 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-10">
 						<div className="mx-auto max-w-2xl lg:mx-0 lg:flex-auto">
 							<div className="flex">
-								{/* "We're hiring" section */}
-								{/* <div className="relative flex items-center gap-x-4 rounded-full px-4 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-									<span className="font-semibold text-indigo-600">
-										We’re hiring
-									</span>
-									<span
-										className="h-4 w-px bg-gray-900/10"
-										aria-hidden="true"
-									/>
-									<a href="#" className="flex items-center gap-x-1">
-										<span className="absolute inset-0" aria-hidden="true" />
-										See open positions
-										<ChevronRightIcon
-											className="-mr-2 h-5 w-5 text-gray-400"
-											aria-hidden="true"
-										/>
-									</a>
-								</div> */}
 							</div>
 							<h1 className="mt-10 max-w-lg text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
 								Take your court to the next level.
@@ -181,14 +163,14 @@ export default function HomePage() {
 									href="#"
 									className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 								>
-									Get started
+									Coming soon.
 								</a>
-								<a
+								{/* <a
 									href="#"
 									className="text-sm font-semibold leading-6 text-gray-900"
 								>
 									Learn more <span aria-hidden="true">→</span>
-								</a>
+								</a> */}
 							</div>
 						</div>
 						<div className="mt-16 sm:mt-24 lg:mt-0 lg:flex-shrink-0 lg:flex-grow">

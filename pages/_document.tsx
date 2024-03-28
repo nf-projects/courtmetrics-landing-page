e6bfdc2b6075f3894395f8d90fb19156000fdd57
nf-projects <90344282+nf-projects@users.noreply.github.com>
2024-03-27 18:32:08 -0700

    Remove Clarity session analytics and Crisp customer chat scripts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,23 +12,6 @@ export default function Document() {
                       href="/favicon-16x16.png"/>
                 <link rel="manifest" href="/site.webmanifest"/>
 
-                {/*clarity session analytics*/}
-                <script type="text/javascript" dangerouslySetInnerHTML={{
-                    __html: `
-                    (function(c,l,a,r,i,t,y){
-                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                    })(window, document, "clarity", "script", "fzjffee4bd");
-                `
-                }}></script>
-
-                {/*crisp customer chat*/}
-                <script type="text/javascript" dangerouslySetInnerHTML={{
-                    __html: `
-                    window.$crisp=[];window.CRISP_WEBSITE_ID="8429e8b2-8afe-4b7f-843a-b5aacd8a0a7d";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
-                `
-                }}></script>
             </Head>
             <body>
             <Main/>
